fa8c6f8fe4c78efd95c1ac583cb616d0144c694a
Luiz <luiz.goncalves.ext@sascar.com.br>
2018-05-25 14:31:55 -0300

    Change base again

--- src/config/app.js
+++ src/config/app.js
@@ -1,6 +1,5 @@
 const configApp = {
-  // base: process.env.NODE_ENV === "production" ? "/" : "/",
-  base: "/",
+  base: process.env.NODE_ENV === "production" ? "/build/" : "/",
   endpoint: {
     contacts: "https://5ae3b92d34b5970014d2ee37.mockapi.io/contacts"
   }
